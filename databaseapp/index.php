cb1723406f8f507afcdf937f358793b67a23a981
abaairahleigh <abaairahleigh@gmail.com>
2026-03-05 09:36:31 +0800

    removed folder

--- databaseapp/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once "controllers/template.controller.php"; //will enter the controllers folder and get the file controller.php
-$template = new ControllerTemplate(); // class to be instiatied in index here / nameObj = $template
-$template -> ctrTemplate();
-?>
